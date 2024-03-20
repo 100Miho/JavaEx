b07343a6d66e90e21face64ec8654e4a228bf258
gggy <gggyeoung@gmail.com>
2024-03-20 10:23:07 +0900

    240319

--- src/com/javaex/basic/loop/LoopPractice.java
+++ src/com/javaex/basic/loop/LoopPractice.java
@@ -2,12 +2,17 @@ package com.javaex.basic.loop;
 
 public class LoopPractice {
 	public static void main(String[] args) {
-		System.out.println("For문 구구단");
-		forGugu();
-		System.out.println("\nWhile문 구구단");
-		whileGugu();
-		System.out.println("\nDo~While문 구구단");
-		doWhileGugu();
+//		System.out.println("For문 구구단");
+//		forGugu();
+//		System.out.println("\nWhile문 구구단");
+//		whileGugu();
+//		System.out.println("\nDo~While문 구구단");
+//		doWhileGugu();
+		
+		System.out.println("\n\nFor문 ☆");
+		forStar();
+		System.out.println("\n\nWhile문 ☆");
+		whileStar();
 	}
 	
 	public static void forGugu() {
@@ -22,12 +27,12 @@ public class LoopPractice {
 	public static void whileGugu() {
 		int dan=2, num=1;
 		while(dan < 10) {
+			num=1;
 			while(num < 10) {
 				System.out.printf("%d*%d=%d\t", dan, num, dan*num);
 				num++;
 			}
 			System.out.println(dan);
-			num=1;
 			dan++;
 		}
 	}
@@ -43,5 +48,30 @@ public class LoopPractice {
 				num++;
 			} while (num < 10);
 		} while (dan < 9);
+	}
+	
+	public static void forStar() {
+		// 행루프 : 1 ~ 6
+		for (int row=1; row<=6; row++) {
+			// 열루프 : 1 ~ 현재 row
+			for (int col=1; col<=row; col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void whileStar() {
+		int row=1;
+		while(row<=6) {
+			int col=1;
+			// 행루프 : 1 ~ row
+			while(col<=row) {
+				System.out.print("*");
+				col++;
+			}
+			row++;
+			System.out.println();
+		}
 	}
 }
